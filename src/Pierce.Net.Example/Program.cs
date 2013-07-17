b9a40f2e61d6f59aff363961f0c796579dd45fa5
anthony taranto <anthony.taranto@gmail.com>
2013-07-17 02:20:15 -0700

    basic caching

--- src/Pierce.Net.Example/Program.cs
+++ src/Pierce.Net.Example/Program.cs
@@ -2,6 +2,10 @@ using System;
 using Pierce.Net;
 using System.Reactive.Linq;
 using System.Text;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Threading;
 
 namespace Pierce.Net.Example
 {
@@ -12,18 +16,21 @@ namespace Pierce.Net.Example
             var queue = new RequestQueue();
 
             var uri = new Uri("http://www.google.com");
-            var request = new Request
-            {
-                Uri = uri,
-            };
 
-            queue.
-                Add(request).
-                Subscribe(response =>
+            for (var x = 0; x < 8; x++)
             {
-                var @string = Encoding.UTF8.GetString(response.Data);
-                Console.WriteLine(@string);
-            });
+                Thread.Sleep(500);
+                var request = new Request { Uri = uri };
+                var request_number = x;
+
+                queue.
+                    Add(request).
+                    Subscribe(response =>
+                {
+                    var @string = Encoding.UTF8.GetString(response.Data).Substring(0, 64);
+                    Console.WriteLine("{0}: {1}", request_number, @string);
+                });
+            };
 
             Console.ReadLine();
         }
--- src/Pierce/Pierce.Net/IHttpClient.cs
+++ src/Pierce/Pierce.Net/IHttpClient.cs
@@ -2,12 +2,31 @@ using System;
 using System.Net;
 using System.Reactive.Subjects;
 using System.Reactive.Linq;
+using System.Reactive.Concurrency;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Reactive;
+using System.Collections.Generic;
 
 namespace Pierce.Net
 {
-    public class Request
+    public class Request : IObservable<Response>
     {
+        public Request()
+        {
+            ShouldCache = true;
+            Subject = new Subject<Response>();
+        }
+
         public Uri Uri { get; set; }
+        public uint Sequence { get; set; }
+        public bool ShouldCache { get; set; }
+        public ISubject<Response> Subject { get; private set; }
+
+        public IDisposable Subscribe(IObserver<Response> observer)
+        {
+            return Subject.Subscribe(observer);
+        }
     }
 
     public class Response
@@ -16,47 +35,110 @@ namespace Pierce.Net
         public Exception Exception { get; set; }
     }
 
-    public class RequestExecutor
+    public class Network
     {
-        public Request Execute(Request request, ISubject<Response> subject)
+        public Response Execute(Request request)
         {
-            var client = new WebClient();
+            var response = new Response();
 
-            client.DownloadDataCompleted += client_DownloadDataCompleted;
-            client.DownloadDataAsync(request.Uri, subject);
+            try
+            {
+                var client = new WebClient();
+                response.Data = client.DownloadData(request.Uri);
+            }
+            catch (Exception ex)
+            {
+                response.Exception = ex;
+            }
 
-            return request;
+            return response;
+        }
+    }
+
+    public class Cache
+    {
+        private readonly IDictionary<object, byte[]> _dictionary = new Dictionary<object, byte[]>();
+
+        public bool TryGet(object key, out byte[] entry)
+        {
+            lock (_dictionary)
+            {
+                return _dictionary.TryGetValue(key, out entry);
+            }
         }
 
-        private void client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        public void Put(object key, byte[] entry)
         {
-            var observer = e.UserState as IObserver<Response>;
-            var response = new Response
+            lock (_dictionary)
             {
-                Data = e.Result,
-                Exception = e.Error,
-            };
-
-            observer.OnNext(response);
-            observer.OnCompleted();
+                _dictionary[key] = entry;
+            }
         }
     }
 
     public class RequestQueue
     {
-        private readonly RequestExecutor _executor;
+        private readonly BlockingCollection<Request> _cache_queue = new BlockingCollection<Request>();
+        private readonly BlockingCollection<Request> _network_queue = new BlockingCollection<Request>();
 
-        public RequestQueue(RequestExecutor executor = null)
+        private readonly Cache _cache;
+        private readonly Network _network;
+
+        public RequestQueue(Cache cache = null, Network network = null)
         {
-            _executor = executor ?? new RequestExecutor();
+            _cache = cache ?? new Cache();
+            _network = network ?? new Network();
+
+            Task.Factory.StartNew(CacheConsumer);
+            Task.Factory.StartNew(NetworkConsumer);
         }
 
-        public IObservable<Response> Add(Request request)
+        public Request Add(Request request)
         {
-            var subject = new Subject<Response>();
-            _executor.Execute(request, subject);
+            if (request.ShouldCache)
+            {
+                _cache_queue.Add(request);
+            }
+            else
+            {
+                _network_queue.Add(request);
+            }
 
-            return subject;
+            return request;
+
+        }
+
+        private void CacheConsumer()
+        {
+            foreach (var request in _cache_queue.GetConsumingEnumerable())
+            {
+                byte[] data;
+                if (_cache.TryGet(request.Uri, out data))
+                {
+                    var response = new Response { Data = data };
+                    request.Subject.OnNext(response);
+                }
+                else
+                {
+                    _network_queue.Add(request);
+                }
+            }
+        }
+
+        private void NetworkConsumer()
+        {
+            foreach (var request in _network_queue.GetConsumingEnumerable())
+            {
+                Console.WriteLine("_network.Execute: {0}", request.Uri);
+                var response = _network.Execute(request);
+
+                if (request.ShouldCache)
+                {
+                    _cache.Put(request.Uri, response.Data);
+                }
+
+                request.Subject.OnNext(response);
+            }
         }
     }
 }
