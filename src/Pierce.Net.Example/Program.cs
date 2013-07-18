f78e6684640405a78f245d20406baf275d1b8262
anthony taranto <anthony.taranto@gmail.com>
2013-07-18 03:06:35 -0700

    limit in flight requests for the same url, respond to subsequent requests with cached response

--- src/Pierce.Net.Example/Program.cs
+++ src/Pierce.Net.Example/Program.cs
@@ -1,8 +1,6 @@
 using System;
 using Pierce.Net;
-using System.Reactive.Linq;
 using System.Text;
-using System.Reactive.Subjects;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Threading;
@@ -15,20 +13,27 @@ namespace Pierce.Net.Example
         {
             var queue = new RequestQueue();
 
-            var uri = new Uri("http://www.google.com");
+            var google = new Uri("http://www.google.com");
+            var yahoo = new Uri("http://www.yahoo.com");
 
-            for (var x = 0; x < 8; x++)
+            for (var x = 1; x <= 16; x++)
             {
-                Thread.Sleep(500);
-                var request = new Request { Uri = uri };
-                var request_number = x;
+                Thread.Sleep(200);
 
-                queue.
-                    Add(request).
-                    Subscribe(response =>
+                var request_number = x;
+                var uri = x % 2 == 0 ?
+                    google :
+                    yahoo;
+
+                queue.Add(new Request
                 {
-                    var @string = Encoding.UTF8.GetString(response.Data).Substring(0, 64);
-                    Console.WriteLine("{0}: {1}", request_number, @string);
+                    Uri = uri,
+                    OnResponse = response =>
+                    {
+                        var @string = Encoding.UTF8.GetString(response.Data).Substring(0, 64);
+                        //Console.WriteLine("{0}: {1}", request_number, @string);
+                        Console.WriteLine("received response {0}", request_number);
+                    },
                 });
             };
 
--- src/Pierce/Pierce.Net/IHttpClient.cs
+++ src/Pierce/Pierce.Net/IHttpClient.cs
@@ -1,31 +1,27 @@
 using System;
 using System.Net;
-using System.Reactive.Subjects;
-using System.Reactive.Linq;
-using System.Reactive.Concurrency;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
-using System.Reactive;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Pierce.Net
 {
-    public class Request : IObservable<Response>
+    public class Request
     {
         public Request()
         {
             ShouldCache = true;
-            Subject = new Subject<Response>();
         }
 
         public Uri Uri { get; set; }
-        public uint Sequence { get; set; }
+        public int Sequence { get; set; }
         public bool ShouldCache { get; set; }
-        public ISubject<Response> Subject { get; private set; }
+        public Action<Response> OnResponse { get; set; }
 
-        public IDisposable Subscribe(IObserver<Response> observer)
+        public override string ToString()
         {
-            return Subject.Subscribe(observer);
+            return String.Format("{0}:{1}", Sequence, Uri);
         }
     }
 
@@ -80,9 +76,12 @@ namespace Pierce.Net
     {
         private readonly BlockingCollection<Request> _cache_queue = new BlockingCollection<Request>();
         private readonly BlockingCollection<Request> _network_queue = new BlockingCollection<Request>();
+        private readonly IDictionary<Uri, List<Request>> _requests = new Dictionary<Uri, List<Request>>();
 
         private readonly Cache _cache;
         private readonly Network _network;
+
+        private int sequence;
 
         public RequestQueue(Cache cache = null, Network network = null)
         {
@@ -90,22 +89,41 @@ namespace Pierce.Net
             _network = network ?? new Network();
 
             Task.Factory.StartNew(CacheConsumer);
+
+            Task.Factory.StartNew(NetworkConsumer);
             Task.Factory.StartNew(NetworkConsumer);
         }
 
         public Request Add(Request request)
         {
-            if (request.ShouldCache)
-            {
-                _cache_queue.Add(request);
-            }
-            else
+            request.Sequence = Interlocked.Increment(ref sequence);
+            Console.WriteLine("Add(): {0}", request);
+
+            if (!request.ShouldCache)
             {
                 _network_queue.Add(request);
+                return request;
+            }
+
+            var key = request.Uri;
+            lock (_requests)
+            {
+                List<Request> list;
+                if (_requests.TryGetValue(key, out list))
+                {
+                    list = list ?? new List<Request>();
+                    list.Add(request);
+
+                    _requests[key] = list;
+                }
+                else
+                {
+                    _requests[key] = null;
+                    _cache_queue.Add(request);
+                }                
             }
 
             return request;
-
         }
 
         private void CacheConsumer()
@@ -116,7 +134,7 @@ namespace Pierce.Net
                 if (_cache.TryGet(request.Uri, out data))
                 {
                     var response = new Response { Data = data };
-                    request.Subject.OnNext(response);
+                    Complete(request, response);
                 }
                 else
                 {
@@ -129,7 +147,8 @@ namespace Pierce.Net
         {
             foreach (var request in _network_queue.GetConsumingEnumerable())
             {
-                Console.WriteLine("_network.Execute: {0}", request.Uri);
+                Console.WriteLine("NetworkConsumer: {0}", request);
+
                 var response = _network.Execute(request);
 
                 if (request.ShouldCache)
@@ -137,8 +156,29 @@ namespace Pierce.Net
                     _cache.Put(request.Uri, response.Data);
                 }
 
-                request.Subject.OnNext(response);
+                Complete(request, response);
             }
+        }
+
+        private void Complete(Request request, Response response)
+        {
+            if (request.ShouldCache)
+            {
+                var key = request.Uri;
+                List<Request> list;
+
+                lock (_requests)
+                {
+                    if (_requests.TryGetValue(key, out list) &&
+                        _requests.Remove(key) &&
+                        list != null)
+                    {
+                        list.ForEach(_cache_queue.Add);
+                    }
+                }
+            }
+
+            request.OnResponse(response);
         }
     }
 }
