c80818e7a23a213bf1eb5be2bedde757c1ca87f9
anthony taranto <anthony.taranto@gmail.com>
2013-08-08 01:09:15 -0700

    add RetryPolicy

--- src/Pierce/Pierce.Net/IHttpClient.cs
+++ src/Pierce/Pierce.Net/IHttpClient.cs
@@ -27,6 +27,33 @@ namespace Pierce.Net
         }
     }
 
+    public class RetryPolicy
+    {
+        public int CurrentRetryCount { get; private set; }
+        public int CurrentTimeoutMs { get; private set; } // XXX TimeSpan?
+
+        private readonly int _max_retries;
+        private readonly double _backoff_multiplier;
+
+        public RetryPolicy(int initial_timeout_ms = 2500, int max_retries = 1, double backoff_multiplier = 1)
+        {
+            CurrentTimeoutMs = initial_timeout_ms;
+            _max_retries = max_retries;
+            _backoff_multiplier = backoff_multiplier;
+        }
+
+        public void Retry(Error error)
+        {
+            CurrentRetryCount++;
+            CurrentTimeoutMs += (int)Math.Round(CurrentTimeoutMs * _backoff_multiplier);
+
+            if (CurrentRetryCount > _max_retries)
+            {
+                throw error;
+            }
+        }
+    }
+
     public abstract class Request
     {
         public Request()
