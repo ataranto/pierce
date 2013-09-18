3164a72299e1378b1ba84e0ca958f83f7d2b701f
anthony taranto <anthony.taranto@gmail.com>
2013-09-18 02:06:53 -0700

    add Assert.ThrowsAsync<T>()

--- src/Pierce.Test/Pierce.Net.Test/RequestFixture.cs
+++ src/Pierce.Test/Pierce.Net.Test/RequestFixture.cs
@@ -1,16 +1,17 @@
 using System;
+using System.Threading.Tasks;
 using Xunit;
-using Pierce.Test;
 
 namespace Pierce.Net.Test
 {
     public class RequestFixture
     {
         [Fact]
-        public void GetResultAsync_WhenNotAddedToQueue_ThrowsException()
+        public async Task GetResultAsync_NotAddedToQueue_ThrowsException()
         {
             var request = new StringRequest();
-            Assert.Throws<InvalidOperationException>(() => request.GetResultAsync());
+            await Pierce.Test.Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                await request.GetResultAsync());
         }
     }
 }
---
+++ src/Pierce.Test/Pierce.Test/Assert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Pierce.Test
+{
+    public static class Assert
+    {
+        public static void Fail(string message = null)
+        {
+            Xunit.Assert.True(false, message);
+        }
+
+        // http://stackoverflow.com/a/14103924/64290
+        public static async Task<Exception> ThrowsAsync<T>(Func<Task> func)
+        {
+            try
+            {
+                await func();
+                Assert.Fail("No exception was thrown");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Xunit.Assert.Equal(typeof(T), ex.GetType());
+                return ex;
+            }
+        }
+    }
+}
