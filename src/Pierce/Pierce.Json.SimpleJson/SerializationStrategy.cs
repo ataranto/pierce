a2f1b4d18a1d54f75a03a13da1fa5826e43d031d
anthony taranto <anthony.taranto@gmail.com>
2013-11-07 17:50:01 -0800

    SerializationStrategy: use ToLowerInvariant()

--- src/Pierce/Pierce.Json.SimpleJson/SerializationStrategy.cs
+++ src/Pierce/Pierce.Json.SimpleJson/SerializationStrategy.cs
@@ -7,7 +7,7 @@ namespace Pierce.Json.SimpleJson
         protected override string MapClrMemberNameToJsonFieldName(string clrPropertyName)
         {
             // https://github.com/jonnii/chinchilla/blob/master/src/Chinchilla.Api/RabbitJsonSerializerStrategy.cs
-            return Regex.Replace(clrPropertyName, "([a-z])([A-Z])", "$1_$2").ToLower();
+            return Regex.Replace(clrPropertyName, "([a-z])([A-Z])", "$1_$2").ToLowerInvariant();
         }
     }
 }
